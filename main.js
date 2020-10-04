f4dbac5fb4070efd15a4420b357df9e7cadcdf15
Paige <paigegryski@gmail.com>
2020-10-04 16:36:18 -0400

    function

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ var pizza = {
     }
 };
 
+function orderPizza() {
+    console.log(pizza.crust);
+    console.log(pizza.cheese);
+    console.log(pizza.sauce);
+    console.log(pizza.addToppings());
+}
 
+orderPizza(pizza);
